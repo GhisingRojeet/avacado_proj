30357bcad7ad1dedbcb001fe39b12b607e1a41ca
Rojeet Ghising <rojeetghising@Rojeets-MacBook-Air.local>
2023-11-02 18:38:15 +0545

    post modified

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -9,6 +9,7 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  final demo = "The sun dipped below the horizon, casting a warm, golden glow across the tranquil lake. Birds chirped in the nearby trees, and a gentle breeze rustled the leaves. The aroma of freshly bloomed flowers filled the air, creating a sense of serenity and peace. As the day came to a close, the world seemed to slow down, and all that remained was the beauty of nature and the promise of a new day.";
   final videoUrl = "https://www.youtube.com/watch?v=vx4kLgnFexo";
   late YoutubePlayerController?_controller;
   @override
@@ -34,45 +35,48 @@ class _PostScreenState extends State<PostScreen> {
         title: Text("Post",style: TextStyle(color: Colors.white,fontSize: 30),),
 
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [YoutubePlayer(controller: _controller!,
-        showVideoProgressIndicator: true,),
-        SizedBox(height: 5.0,),
-        Padding(
-          padding: const EdgeInsets.only(left: 30,right: 20,top: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                height: 30,
-                width: 130,
-                decoration: BoxDecoration(color: Colors.red,
-                borderRadius: BorderRadius.circular(20.0)),
-                child: Row(children: [
-                  SizedBox(width: 10.0,),
-                  Icon(Icons.arrow_forward_ios,color: Colors.white,),
-                                  SizedBox(width: 20.0,),
-        
-                  Text('YouTube',style: TextStyle(color: Colors.white),)
-                ],),
-              ),
-              Text('1 hour ago')
-            ],
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [YoutubePlayer(controller: _controller!,
+          showVideoProgressIndicator: true,),
+          SizedBox(height: 5.0,),
+          Padding(
+            padding: const EdgeInsets.only(left: 30,right: 20,top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 30,
+                  width: 130,
+                  decoration: BoxDecoration(color: Colors.red,
+                  borderRadius: BorderRadius.circular(20.0)),
+                  child: Row(children: [
+                    SizedBox(width: 10.0,),
+                    Icon(Icons.arrow_forward_ios,color: Colors.white,),
+                                    SizedBox(width: 20.0,),
           
-        ),
-        SizedBox(height: 20,),
-        Padding(
-          padding: const EdgeInsets.only(left:15.0),
-          child: Container(
-            child:Column(children: [
-               Text("Mitski - My Love Mine All Mine (Official Video)",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-               
-            ],)
+                    Text('YouTube',style: TextStyle(color: Colors.white),)
+                  ],),
+                ),
+                Text('1 hour ago')
+              ],
+            ),
+            
           ),
-        )
-        ]
+          SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.only(left:15.0),
+            child: Container(
+              child:Column(children: [
+                 Text("Mitski - My Love Mine All Mine (Official Video)\n ${demo} ",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                
+                 
+              ],)
+            ),
+          )
+          ]
+        ),
       ),);
     
     
